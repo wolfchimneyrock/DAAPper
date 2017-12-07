8f53a22e718df9a9e01def5d57e9d13969eb89f8
Robert Wagner <tholos@gmail.com>
2017-12-07 13:53:33 -0500

    built version for each allocator to test

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-LIBRARY_PATH=/usr/local/lib gcc  -DSQLITE_CORE -DSQLITE_THREADSAFE=2 -O3 -std=gnu99 -I/usr/local/include -I/usr/local/include/evhtp *.c -pthread -ltcmalloc -lfswatch -levent -lsqlite3 -levhtp -lconfuse -levent -o daap
+LIBRARY_PATH=/usr/local/lib gcc  -DSQLITE_CORE -DSQLITE_THREADSAFE=2 -O3 -std=gnu99 -I/usr/local/include -I/usr/local/include/evhtp *.c -pthread  -lfswatch -levent -lsqlite3 -levhtp -lconfuse -levent -o daap-gnu
+LIBRARY_PATH=/usr/local/lib gcc  -DSQLITE_CORE -DSQLITE_THREADSAFE=2 -O3 -std=gnu99 -I/usr/local/include -I/usr/local/include/evhtp *.c -pthread  -ltcmalloc -lfswatch -levent -lsqlite3 -levhtp -lconfuse -levent -o daap-tcmalloc
+LIBRARY_PATH=/usr/local/lib gcc  -DSQLITE_CORE -DSQLITE_THREADSAFE=2 -O3 -std=gnu99 -I/usr/local/include -I/usr/local/include/evhtp *.c -pthread  -ljemalloc -lfswatch -levent -lsqlite3 -levhtp -lconfuse -levent -o daap-jemalloc
